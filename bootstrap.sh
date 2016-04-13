b4eedf8c984f28ee9d0ca0889292f6692c0728e4
dev <dev@penguinbeam.uk>
2016-04-13 20:58:43 +0000

    Removed OS specific bits and added wget install.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#Install Ansible
+# yum install -y git
+# git clone https://github.com/penguinbeam/ansible_mysql-cluster-allinone.git
+# cd ansible_mysql-cluster-allinone/
+# /bin/bash ansible_mysql-cluster-allinone/bootstrap.sh 
 
 yum install -y epel-release
 yum install -y ansible
-
+ansible-playbook -i hosts.ini mysql-cluster.yaml
